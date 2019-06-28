3cef62b38c6836a325e2d49a9ae3e043810bd1a4
Aliaksei Biazverkhi <wertual@gmail.com>
2019-06-28 17:07:19 +0300

    Task 3 new ckass

--- src/Jd1june2019/Lection04/Task3_Rectangle.java
+++ src/Jd1june2019/Lection04/Task3_Rectangle.java
@@ -6,5 +6,11 @@ public class Task3_Rectangle {
     Task3_Point point1;
     Task3_Point point2;
 
+    public int area(Task3_Point point1, Task3_Point point2) {
+        int area = 0;
+
+
+        return area;
+    }
 
 }
