5807d5ae4b550d91c6c730f97bb89641fbf7211f
wertual <wertual@gmail.com>
2019-07-19 18:09:49 +0300

    Начало изучения потоков

--- src/Jd1june2019/Lection15/Task1.java
+++ src/Jd1june2019/Lection15/Task1.java
@@ -1,4 +1,18 @@
 package Jd1june2019.Lection15;
 
+import java.io.*;
+import java.nio.charset.Charset;
+
 public class Task1 {
-}
+
+    public static void main(String[] args) throws IOException {
+        File file =new File("C:"+File.separator+"Users"+File.separator+"L"+File.separator+"Downloads"+File.separator+"myfile.txt");
+
+     FileReader read=new FileReader(file, UTF-8);
+     int ch=read.read();
+     while ((ch=read.read())!=-1)
+     {
+         System.out.println((char)ch);
+    }
+
+}}
