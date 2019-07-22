df3ad2d14fd2fd589947c416276a5a8d792d78e5
Blue <wertual@gmail.com>
2019-07-23 01:26:36 +0300

    17 начало

---
+++ src/Jd1june2019/Lection17/Task1.java
@@ -0,0 +1,17 @@
+package Jd1june2019.Lection17;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        while (true)//для удобства теста
+        {
+            Scanner cs = new Scanner(System.in);
+            String text = cs.nextLine();
+            boolean mail = text.matches("\\b[a-zA-Z][\\w]*@[\\w]*\\.(com|org)\\b");
+            System.out.println(mail);
+        }
+    }
+
+
+}
---
+++ src/Jd1june2019/Lection17/Task2.java
@@ -0,0 +1,16 @@
+package Jd1june2019.Lection17;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String text = "";
+        Pattern pat = Pattern.compile("");
+        Matcher math = pat.matcher(text);
+        if (math.find()) {
+            System.out.println(math.group());
+
+        }
+    }
+}
