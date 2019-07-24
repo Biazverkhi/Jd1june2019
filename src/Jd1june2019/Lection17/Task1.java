030348a5f3b83e8f8fedb64d500195f14b7feabf
wertual <wertual@gmail.com>
2019-07-24 21:29:51 +0300

    17 1-я

---
+++ src/Jd1june2019/Lection17/Task1.java
@@ -0,0 +1,20 @@
+package Jd1june2019.Lection17;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+       List<Integer> list= Arrays.asList(12,15,56,55,78,56,55,47);
+               OptionalDouble sum=list.stream()
+                .filter(element -> element %2 !=0||element %5 ==0)
+                .mapToDouble(element -> element)
+                .average();
+    }
+
+
+
+
+}
